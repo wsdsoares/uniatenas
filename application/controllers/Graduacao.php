5511521c8ccc4e48a4c07ca15a2e3795d356f136
Willian Soares Damasceno <wilhaods@gmail.com>
2024-06-19 15:59:16 -0300

    Update Graduacao.php
    Correção do item da página de EAD, para não mostrar o curso de pós graduação.

--- application/controllers/Graduacao.php
+++ application/controllers/Graduacao.php
@@ -102,7 +102,7 @@ class Graduacao extends CI_Controller
                         inner join courses on courses.id = campus_has_courses.courses_id
                         INNER JOIN courses_pages on courses_pages.campus_has_courses_id = campus_has_courses.id
                         
-                        WHERE courses.types not in ("PosGraduacao")
+                        WHERE courses.types ("PosGraduacao")
                         and courses.id =' . $cursos[$i]->id . " 
                         order by campus.city";
 
@@ -454,7 +454,7 @@ class Graduacao extends CI_Controller
     $colunasTabelaCampus = array('campus.name', 'campus.id', 'campus.city', 'campus.shurtName');
     $dataCampus = $this->bancosite->where($colunasTabelaCampus, 'campus', NULL, array('shurtName' => $uricampus))->row();
 
-    $queryCursos = "SELECT * FROM courses where modalidade = 'ead' and types = 'ead' order by areas_id, name";
+    $queryCursos = "SELECT * FROM courses where modalidade = 'ead' and types = 'ead' and courses.types not in ('PosGraduacao') order by areas_id, name";
 
     $cursos = $this->bancosite->getQuery($queryCursos)->result();
     $polosEad = $this->bancosite->getWhere('polos')->result();
@@ -478,7 +478,8 @@ class Graduacao extends CI_Controller
 
     $whereInformacoesCurso = array(
       'modalidade' => 'ead',
-      'campus.id' => $dataCampus->id
+      'campus.id' => $dataCampus->id,
+      'courses.types' => 'ead'
     );
 
     $campusCursos = $this->bancosite->where($colunasInformacoesCurso, 'courses_pages', $joinInformacoesCursos, $whereInformacoesCurso, array('campo' => 'courses.name', 'ordem' => 'asc'))->result();
