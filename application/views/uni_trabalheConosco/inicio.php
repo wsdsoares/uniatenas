f82551e913ec43587651715ed4936a43c08416b9
Willian Soares Damasceno <wilhaods@gmail.com>
2024-10-17 14:25:01 -0300

    Trabalhe conosco - Finalização

--- application/views/uni_trabalheConosco/inicio.php
+++ application/views/uni_trabalheConosco/inicio.php
@@ -5,23 +5,15 @@ $uricampus = $this->uri->segment(3);
   <div class="container">
     <h2 class="text"><?php echo 'Trabalhe Conosco - ' . $campus->name . ' - ' . $campus->city; ?></h2>
     <div class="row">
-      <?php
-      // echo '<pre>';
-      // print_r($conteudoPag);
-      // echo '<-->';
-      // print_r($imagemLGPD);
-      // print_r($linkRedir);
-      // echo '</pre>';
-      ?>
       <style>
-        .panelResume {
-          background: #16A085;
-        }
+      .panelResume {
+        background: #16A085;
+      }
 
-        .panelResume span {
-          color: #000;
-          font-weight: bold;
-        }
+      .panelResume span {
+        color: #000;
+        font-weight: bold;
+      }
       </style>
       <br />
 
@@ -35,76 +27,76 @@ $uricampus = $this->uri->segment(3);
           <?php
           if (!empty($conteudoPag)) {
           ?>
-            <div class="col-xs-12 panelsite">
-              <p><?php echo $conteudoPag[0]->description; ?></p>
-              <p class="text-center">
+          <div class="col-xs-12 panelsite">
+            <p><?php echo $conteudoPag[0]->description; ?></p>
+            <p class="text-center">
 
-              <div class="">
+            <div class="">
 
-                <body onselectstart='return false' ondragstart='return false' oncontextmenu='return false'>
-                  <div class="container">
-                    <div class="text-center">
-                      <?php
-                      if (isset($imagemLGPD)) {
-                      ?>
-                        <button type="button" class="btnEdital" data-toggle="modal" data-target="#myModal">
-                          Cadastrar Curriculum
-                        </button>
-                      <?php
-                      }
-                      ?>
-                    </div>
+              <body onselectstart='return false' ondragstart='return false' oncontextmenu='return false'>
+                <div class="container">
+                  <div class="text-center">
                     <?php
+                      // if (!empty($imagemLGPD)) {
+                      ?>
+                    <button type="button" class="btnEdital" data-toggle="modal" data-target="#myModal">
+                      Cadastrar Curriculum
+                    </button>
+                    <?php
+                      // }
+                      ?>
+                  </div>
+                  <?php
                     if (!empty($imagemLGPD)) {
                     ?>
-                      <div id="myModal" class="modal fade" role="dialog">
-                        <div class="modal-dialog modal-lg">
-                          <div class="modal-content">
-                            <div class="modal-header">
-                              <button type="button" class="close" data-dismiss="modal">&times;</button>
-                              <h4 class="modal-title">Termo de aceite - LGPD</h4>
-                            </div>
-                            <div class="modal-body">
-                              <?php foreach ($imagemLGPD as $imagem) { ?>
+                  <div id="myModal" class="modal fade" role="dialog">
+                    <div class="modal-dialog modal-lg">
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <button type="button" class="close" data-dismiss="modal">&times;</button>
+                          <h4 class="modal-title">Termo de aceite - LGPD</h4>
+                        </div>
+                        <div class="modal-body">
+                          <?php foreach ($imagemLGPD as $imagem) { ?>
 
-                                <img src='<?php echo base_url($imagem->file); ?>' width="100%" height="100%" />
+                          <img src='<?php echo base_url($imagem->file); ?>' width="100%" height="100%" />
 
-                              <?php } ?>
-                              <div class="modal-footer">
-                                <input type="checkbox" id="aceito" name="aceito" value="Aceito o Termo">
-                                <label for="aceito"> Aceito termo de Uso do Portal RM Banco de Talentos
-                                </label><br /><br />
-                                <button type="button" class="btnEdital" data-dismiss="modal" id="confirmar"
-                                  style="visibility: hidden;"
-                                  onclick="window.location='<?php echo $linkRedir->link_redir ?>'">Confirmar</button>
-                                <button type="button" class="btnEdital" data-dismiss="modal" id="Fechar"
-                                  tyle="background:red !important; border-color:red !important;">Fechar</button>
-                              </div>
-
-                            </div>
+                          <?php } ?>
+                          <div class="modal-footer">
+                            <input type="checkbox" id="aceito" name="aceito" value="Aceito o Termo">
+                            <label for="aceito"> Aceito termo de Uso do Portal RM Banco de Talentos
+                            </label><br /><br />
+                            <button type="button" class="btnEdital" data-dismiss="modal" id="confirmar"
+                              style="visibility: hidden;"
+                              onclick="window.location='<?php echo $linkRedir->link_redir ?>'">Confirmar</button>
+                            <button type="button" class="btnEdital" data-dismiss="modal" id="Fechar"
+                              tyle="background:red !important; border-color:red !important;">Fechar</button>
                           </div>
+
                         </div>
                       </div>
-                    <?php } ?>
+                    </div>
                   </div>
-              </div>
-              <script>
-                var aceite = document.getElementById("aceito");
-                document.getElementById('aceito')
-                  .addEventListener('click', function(event) {
-                    if (aceite.checked) {
-                      document.getElementById('confirmar').style.visibility = "visible";
-                    } else {
-                      document.getElementById('confirmar').style.visibility = "hidden";
-                    }
-                  });
-              </script>
-              </body>
+                  <?php } ?>
+                </div>
             </div>
+            <script>
+            var aceite = document.getElementById("aceito");
+            document.getElementById('aceito')
+              .addEventListener('click', function(event) {
+                if (aceite.checked) {
+                  document.getElementById('confirmar').style.visibility = "visible";
+                } else {
+                  document.getElementById('confirmar').style.visibility = "hidden";
+                }
+              });
+            </script>
+            </body>
+          </div>
 
-            </p>
+          </p>
         </div>
-      <?php
+        <?php
           }
       ?>
       </div>
